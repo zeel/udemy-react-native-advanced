b8574d0ce90f82af956eb62844215d6bb905b078
zeel <kshah215@gmail.com>
2025-11-23 12:32:33 +0530

    moving ball with touch

--- app/Ball.tsx
+++ app/Ball.tsx
@@ -1,14 +1,39 @@
-import { Animated, StyleSheet, View } from "react-native";
+import { useEffect, useRef } from "react";
+import { Animated, PanResponder, StyleSheet, View } from "react-native";
 
 export default function Ball() {
-  const position = new Animated.ValueXY({ x: 0, y: 0 });
-  Animated.spring(position, {
-    toValue: { x: 200, y: 500 },
-    useNativeDriver: false,
-  }).start();
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+
+  useEffect(() => {
+    Animated.spring(position, {
+      toValue: { x: 200, y: 500 },
+      useNativeDriver: false,
+    }).start();
+  }, [position]);
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        position.setOffset({
+          x: (position.x as any)._value,
+          y: (position.y as any)._value,
+        });
+        position.setValue({ x: 0, y: 0 });
+      },
+      onPanResponderMove: Animated.event(
+        [null, { dx: position.x, dy: position.y }],
+        { useNativeDriver: false }
+      ),
+      onPanResponderRelease: () => {
+        position.flattenOffset();
+      },
+    })
+  ).current;
 
   return (
-    <Animated.View style={position.getLayout()}>
+    <Animated.View style={position.getLayout()} {...panResponder.panHandlers}>
       <View style={styles.ball}></View>
     </Animated.View>
   );
